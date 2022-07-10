f86719329aa931259580d565e4cc4e514a643656
shahtag <shashwatsatna@gmail.com>
2022-07-10 19:55:58 +0530

    💩 cypress

---
+++ cypress/e2e/pagination_spec.ts
@@ -0,0 +1,3 @@
+describe("pagination", () => {
+  it("");
+});
--- src/Components/Page.test.tsx
+++ src/Components/Page.test.tsx
@@ -1,3 +1,5 @@
+// import rende from "@testing-library/react"
+
 // test("prev is disabled on page equals to one", { render(<Page/>)});
 
 export {};
