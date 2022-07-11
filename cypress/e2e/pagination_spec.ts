0a6adbb1639123d773bd606d18070329d0db079d
shahtag <shashwatsatna@gmail.com>
2022-07-11 05:32:58 +0530

    🤡  another try at testing

--- cypress/e2e/pagination_spec.ts
+++ cypress/e2e/pagination_spec.ts
@@ -1,3 +1,9 @@
 describe("pagination", () => {
-  it("");
-});
+  it("test if pagination works properly", () => {});
+  // on load user is on page one of all results
+  // check if prev is disabled
+  // click on next for 1-10 times
+  // click on any page except last one
+  // go to the last page
+  // check if next is disbled
+};);
